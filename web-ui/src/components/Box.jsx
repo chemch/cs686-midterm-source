bb5a7cea1d7b52c89fb406ef78b7091d6ae2a9dd
Chase Chemero <cchemero@dons.usfca.edu>
2025-02-24 19:31:43 -0800

    All works.

---
+++ web-ui/src/components/Box.jsx
@@ -0,0 +1,13 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+
+// creation of the parent functional component which will be passed to app.jsx
+function Box() {
+  return (
+    <div>
+      Box Working!
+    </div>
+  );
+}
+
+export default Box;
--- web-ui/vite.config.js
+++ web-ui/vite.config.js
@@ -1,10 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import process from "process";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 6666
+    host: '0.0.0.0',
+    port: process.env.CONTAINER_PORT, 
   }
 });
