7382d961c609d84fae865bd5ac0be530ee60fd78
Chase Chemero <cchemero@dons.usfca.edu>
2025-02-25 21:53:15 -0800

    Works Ok.

--- web-ui/vite.config.js
+++ web-ui/vite.config.js
@@ -2,9 +2,13 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import process from "process";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // build: {
+  //   rollupOptions: {
+  //     input: 'public/index.html',
+  //   },
+  // },
   server: {
     host: '0.0.0.0',
     port: process.env.CONTAINER_PORT, 
