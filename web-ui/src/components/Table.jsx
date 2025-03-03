4e1489e1a1da2ff484357ae33fd2d419d574ba02
Chase Chemero <cchemero@dons.usfca.edu>
2025-03-02 20:15:38 -0800

    support(up): Changing to Complete

--- web-ui/src/components/Table.jsx
+++ web-ui/src/components/Table.jsx
@@ -76,7 +76,7 @@ function Table() {
       <Toaster richColors closeButton />
       <div className="container mt-5">
         <h1 className="mb-4" id="h1">
-          User Table
+          User Table - COMPLETE
         </h1>
         <CreateUserModal addUser={addUser} />
         <EditUserModal />
